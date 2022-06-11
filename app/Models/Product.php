3c17730d1b31fab2dd0e8448b9e6f46a9a6356be
unknown <mkaroudi@gmail.com>
2022-06-11 14:09:56 +0100

    Add scope filter to ordrs

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -58,8 +58,44 @@ class Product extends Model
 
     public function scopeFilter($query, $filters)
     {
-        if (isset($filters['ref'])) {
-            $query->where('ref', '=', $filters['ref']);
+        if (isset($filters['product_ref_sku']) && !empty($filters['product_ref_sku'])) {
+            $query->where('ref', '=', $filters['product_ref_sku'])->orWhere('sku', '=', $filters['product_ref_sku']);
         }
+
+        if (isset($filters['category'])) {
+            $query->where('category_id', $filters['category']);
+        }
+
+        if (isset($filters['location'])) {
+           $query->where('location_id', $filters['location']);
+        }
+
+        if (isset($filters['supplier'])) {
+            $query->where('supplier_id', $filters['supplier']);
+        }
+
+
+        if (isset($filters['no_cost']) && !empty($filters['no_cost'])) {
+            $no_cost = filter_var($filters['no_cost'], FILTER_VALIDATE_BOOLEAN);
+            if ($no_cost) {
+                $query->where('cost', '=', 0);
+            }
+        }
+
+        if (isset($filters['no_main_ref']) && !empty($filters['no_main_ref'])) {
+            $no_main_ref = filter_var($filters['no_main_ref'], FILTER_VALIDATE_BOOLEAN);
+            if ($no_main_ref) {
+                $query->where('mainRef', '=', null);
+            }
+        }
+
+        if (isset($filters['is_paid']) && !empty($filters['is_paid'])) {
+            $is_paid = filter_var($filters['is_paid'], FILTER_VALIDATE_BOOLEAN);
+            if ($is_paid) {
+                $query->where('is_paid', '=', 1);
+            }
+        }
+
+
     }
 }
