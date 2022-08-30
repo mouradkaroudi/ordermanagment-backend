ab6eacf2539fc003c4bed2e2f1884e6d6ec528ea
unknown <mkaroudi@gmail.com>
2022-08-30 14:01:09 +0100

    fix import

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Models\File;
 use App\Models\Product;
+use Exception;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithUpserts;
@@ -25,18 +26,15 @@ class ProductsImport implements ToModel, WithUpserts
 
     public function model(array $row)
     {
-
-        if($this->current_row > 0 && $row[4]) {
+        if($this->current_row > 0 && isset($row[4]) && isset($row[0])) {
 
             $image = $row[5];
-
             $entries = [
                 'sku' => $row[0],
                 'ref' => trim(strtolower($row[1])),
                 'cost' => $row[2],
                 'name' => $row[4]
             ];
-
             if($image) {
                 
                 $file = File::firstOrCreate([
@@ -49,10 +47,11 @@ class ProductsImport implements ToModel, WithUpserts
 
             }
 
-            $product = Product::withTrashed()->where('ref', trim(strtolower($row[1])))->orWhere('sku', $row[0])->get()[0];
+            $product = Product::withTrashed()->where('ref', trim(strtolower($row[1])))->orWhere('sku', $row[0])->get()->toArray();
             
             if(!empty($product)) {
-                Product::where('id', $product->id)->update([
+                $product = $product[0];
+                Product::where('id', $product['id'])->update([
                     'cost' => $row[2],
                     'name' => $row[4]
                 ]);
